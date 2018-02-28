5010c8fed82909f1dd5df637b3bad5a477386f2d
Hristo Gatsinski <gatsinski@gmail.com>
2018-02-28 22:43:24 +0200

    Add pagination to `Blog` app

--- website/website/contrib/blog/views.py
+++ website/website/contrib/blog/views.py
@@ -8,6 +8,7 @@ from .models import Post
 class PostListView(ListView):
     model = Post
     queryset = Post.objects.published()
+    paginate_by = 10
 
 
 class PostDetailView(TranslatableSlugMixin, DetailView):
