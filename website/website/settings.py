9f8578673e23d92a49630592d5b56d9610bd4c7d
Hristo Gatsinski <gatsinski@gmail.com>
2018-08-23 21:20:57 +0300

    Add dummy `SECRET_KEY`

--- website/website/settings.py
+++ website/website/settings.py
@@ -63,6 +63,8 @@ PIPELINE = {
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
