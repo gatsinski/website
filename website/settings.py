2331eac04e3bdaa97e7252535ba5f594d0fb2635
Hristo Gatsinski <gatsinski@gmail.com>
2018-10-06 10:54:38 +0300

    Add `PostgreSQL` database

--- website/settings.py
+++ website/settings.py
@@ -21,7 +21,6 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_PATH, 'static',),
     os.path.join(BASE_DIR, 'node_modules'),
 )
 
@@ -179,11 +178,14 @@ WSGI_APPLICATION = 'website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_HOST'),
+        'PORT': 5432
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
